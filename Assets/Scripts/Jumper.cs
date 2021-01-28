640eb1246ac2b7383362a7df9682d4c8c9de3ca5
Austin Bennett <austinbennett03@gmail.com>
2021-01-28 14:33:23 -0600

    VR Wuhu Island

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Jumper : MonoBehaviour
+{
+
+    public XRNode inputSource;
+    public Transform theCube;
+
+    [SerializeField] private TeleportationProvider _teleportationProvider;
+    [SerializeField] private bool isTrigger;
+
+    void Start()
+    {
+        _teleportationProvider = GetComponent<TeleportationProvider>();
+
+    }
+
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.triggerButton, out isTrigger);
+
+        if (isTrigger)
+        {
+            Jump();
+        }
+    }
+    private void Jump()
+    {
+        var teleportRequest = new TeleportRequest();
+        teleportRequest.destinationPosition = theCube.position;
+        _teleportationProvider.QueueTeleportRequest(teleportRequest);
+    }
+}
+
+
---
+++ Assets/Scripts/grab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grab : MonoBehaviour
+{
+    int count;
+    
+    public void DoSomething()
+    {
+        count++;
+        Debug.Log(count);
+    }
+}
